d9e4fc2d96fdb75d880bfd9c76158d8da402a13c
Alex Austin <austinalex71@gmail.com>
2025-04-16 20:49:52 -0300

    borrador para el trabajo integrador

---
+++ trabajo_integrador.py
@@ -0,0 +1,26 @@
+#menu para convertir de 1.binario a decimal o 2.decimal a binario o 3.salir del menu
+
+
+
+
+
+
+
+
+# conversor de binario a decimal(de 0 al 9, sino salta error e ingrese nuevamente el numero)
+
+
+
+
+
+
+#conversor de decimal a binario (de 0 al 9, sino salta error e ingrese nuevamente el numero)
+
+
+
+
+
+
+#juego de adivinar el numero y salte cuanto intentos tuvo o q sean con intentos prestablecido (si se acaban qur salga el numero secreto)
+
+
