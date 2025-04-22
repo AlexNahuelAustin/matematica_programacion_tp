f436d0dddb9a5e7c3dc6a5afb99a80aec1df27bb
Leonel <leonel_jesus_90@hotmail.com>
2025-04-22 16:04:36 -0300

    del 0 al 14

--- trabajo_integrador.py
+++ trabajo_integrador.py
@@ -3,14 +3,14 @@ while True:
     opcion = input("ingrese una opcion:\n 1.para conversor a binario\n 2.para el conversor a decimal.\n 3.salir \n ")
     if opcion == "1":
         print("pasaje de decimal a binario")
-        numero = input("ingrese un numero del 0 al 9: ")
+        numero = input("ingrese un numero del 0 al 14: ")
         try:
             numero = int(numero)
-            if 0 <= numero <= 9:
+            if 0 <= numero <= 14:
                 binario_convertido = bin(numero)[2:]
                 print(f"el {numero} en binario es: {binario_convertido}")
             else:
-                print("Por favor, ingrese un número entre 0 y 9: ")
+                print("Por favor, ingrese un número entre 0 y 14: ")
         except ValueError:
             print("Entrada inválida. Por favor, ingrese un número entero.")
     elif opcion == "2":
@@ -18,10 +18,10 @@ while True:
         binario = input("ingrese un numero binario: ")
         try:
             decimal_convertido = int(binario, 2)
-            if 0 <= decimal_convertido <= 9:
+            if 0 <= decimal_convertido <= 14:
                 print(f"El binario {binario} en decimal es: {decimal_convertido}")
             else:
-                print("El número binario ingresado representa un valor fuera del rango 0-9.")
+                print("El número binario ingresado representa un valor fuera del rango 0-14.")
         except ValueError:
             print("Entrada inválida. Por favor, ingrese un número binario válido (solo 0s y 1s).")
     elif opcion == "3":
