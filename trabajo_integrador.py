05003ba4289e7b3f195a04881b387834155b1cdb
Ginoc07 <dacowboy92@gmail.com>
2025-04-24 15:05:40 -0300

    Modificamos el menú y agregamos la opción para el contador binario

--- trabajo_integrador.py
+++ trabajo_integrador.py
@@ -1,5 +1,25 @@
-#menu para convertir de 1.decimal a binario o 2.binario a decimal o 3 Juego 4 Salir
+#Menu para convertir de 1.decimal a binario o 2.binario a decimal o 3 Juego 4 Salir
 import random
+# Creamos la función contador_binario que muestra los números del 0 al 15 y sus respectivos num binarios
+def contador_binario():
+    
+    import time
+
+    print("Estos son los numeros Binarios del 0 al 15")
+    time.sleep(2)
+
+    for i in range(0,16):   
+        num = i
+        time.sleep(0.5)
+        binario = "0"
+
+        while num >= 2:
+            binario += str(num % 2)
+            num = num // 2
+
+        binario += str(num)
+
+        print(f"{i} = {binario[::-1]}")
 
 
 while True: # Se crea el Menu de opciones (While al entrar al True solo toma verdadero)
@@ -7,7 +27,8 @@ while True: # Se crea el Menu de opciones (While al entrar al True solo toma ver
     print("1. Conversor decimal a binario")
     print("2. Conversor binario a decimal")
     print("3. Juego de adivinar el número")
-    print("4. Salir")
+    print("4. Contador binario")
+    print("5. Salir")
     opcion = input("Ingrese su numero ")
     
     if opcion == "1": # Primera opcion del menu
@@ -50,12 +71,16 @@ while True: # Se crea el Menu de opciones (While al entrar al True solo toma ver
                     print("Intenta con un número menor.")
             except ValueError:
                 print("Por favor, ingrese un número entero válido.")
-        
-    elif opcion == "4": # Tercera Opcion del menu
+    elif opcion == "4": # Agregamos la función de contador binario
+        contador_binario()
+
+
+
+    elif opcion == "5": # Tercera Opcion del menu
         print("saliendo")
         break
     else:
-        print("Opción inválida. Por favor, ingrese 1, 2, 3 o 4.")
+        print("Opción inválida. Por favor, ingrese 1, 2, 3, 4 o 5.")
         continue
     
     print("\n¿Querés empezar de nuevo?")
@@ -74,25 +99,4 @@ while True: # Se crea el Menu de opciones (While al entrar al True solo toma ver
 
 #Sistema conversor esta funcionando
 
-# Creamos la función contador_binario que muestra los números del 0 al 15 y sus respectivos num binarios
-def contador_binario():
-    
-    import time
 
-    print("Estos son los numeros Binarios del 0 al 15")
-    time.sleep(2)
-
-    for i in range(0,16):   
-        num = i
-        time.sleep(0.5)
-        binario = "0"
-
-        while num >= 2:
-            binario += str(num % 2)
-            num = num // 2
-
-        binario += str(num)
-
-        print(f"{i} = {binario[::-1]}")
-
-contador_binario()
